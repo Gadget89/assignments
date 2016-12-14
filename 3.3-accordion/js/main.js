6103c959d933ad8973b5ef4547b2ab4a1c4dde97
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-14 16:04:07 -0600

    Added elements to files to make assignment work

--- 3.3-accordion/js/main.js
+++ 3.3-accordion/js/main.js
@@ -1,1 +1,9 @@
 // Main JS //
+$(function(){
+  var $itemElements = $("[data-js='item']");
+  $itemElements.on("click", function(e){
+    var $clickedItem = $(this);
+    $itemElements.filter(".expand").removeClass("expand");
+    $clickedItem.addClass("expand");
+  });
+});
