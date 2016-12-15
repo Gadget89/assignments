12d19719f0849d5bd027ba9cd629239d5b5da61a
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-15 16:07:37 -0600

    Added JS basic functionality to HTML

--- 3.4-jQuery-Holiday-Buying-List/js/main.js
+++ 3.4-jQuery-Holiday-Buying-List/js/main.js
@@ -1,1 +1,23 @@
 // Main JS
+$(function() {
+  var $body = $("body");
+
+  var $textArea = $body.find("[data-js='postForm__text']");
+  var $submitButton = $body.find("[data-js='postForm__submit']");
+  var $postsElement = $body.find("[data-js='posts']");
+
+  $submitButton.on("click", function(e){
+    e.preventDefault();
+    var textAreaValue = $textArea.val();
+
+    var $newItem =$("<p>")
+      .text(textAreaValue)
+    ;
+
+    var $articleHTML = $("<article>")
+      .append($newItem)
+    ;
+
+    $postsElement.prepend($articleHTML)
+  });
+});
