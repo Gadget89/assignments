00a783898adc3cbe4cb3121a0999430a5f21e1b1
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-21 14:40:35 -0600

    Created checklist and completed items on it to set up new assignment with webpack and jquery

---
+++ 4.3-refactor-calculator/dist/bundle.js
@@ -0,0 +1,160 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	//TODO
+
+	// This is the 'addEventListener' for the document
+	document.addEventListener("DOMContentLoaded", function() {
+	  // This is event listener & callback for clearButton
+	  var clearButtonElement = document.querySelector("[data-js='clearButton']");
+	  // This is event listener & callback for display
+	  var displayElement = document.querySelector("[data-js='display']");
+	  clearButtonElement.addEventListener("click", function(){
+	    // This will clear the display when clearButtonElement is clicked
+	    displayElement.textContent = "";
+	    })
+	  // This is event listener & clallback for 7 button
+	  var element7 = document.querySelector("[data-js='7button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element7.addEventListener("click", function(){
+	      displayElement.textContent += 7;
+	    })
+	  // This is event listener & callback for 8 button
+	  var element8 = document.querySelector("[data-js='8button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element8.addEventListener("click", function(){
+	      displayElement.textContent += 8;
+	    })
+	  // This is event listener & callback for 9 button
+	  var element9 = document.querySelector("[data-js='9button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element9.addEventListener("click", function(){
+	      displayElement.textContent += 9;
+	    })
+	  // This is event listener & callback for / button
+	  var elementDivide = document.querySelector("[data-js='/button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    elementDivide.addEventListener("click", function(){
+	      displayElement.textContent += " / ";
+	    })
+	  // This is event listener & callback for 4
+	  var element4 = document.querySelector("[data-js='4button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element4.addEventListener("click", function(){
+	      displayElement.textContent += 4;
+	    })
+	  // This is event listener & callback for 5
+	  var element5 = document.querySelector("[data-js='5button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element5.addEventListener('click', function(){
+	      displayElement.textContent += 5;
+	    })
+	  // This is event listener & callback for 6
+	  var element6 = document.querySelector("[data-js='6button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element6.addEventListener('click', function(){
+	      displayElement.textContent += 6;
+	    })
+	  // This is event listener & callback for X button
+	  var elementMultiply = document.querySelector("[data-js='Xbutton']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    elementMultiply.addEventListener('click', function(){
+	      displayElement.textContent += "*";
+	    })
+	  // This is event listener & callback for 1
+	  var element1 = document.querySelector("[data-js='1button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element1.addEventListener('click', function(){
+	      displayElement.textContent += 1;
+	    })
+	  // This is event listener & callback for 2
+	  var element2 = document.querySelector("[data-js='2button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element2.addEventListener('click', function(){
+	      displayElement.textContent += 2;
+	    })
+	  // This is event listener & callback for 3
+	  var element3 = document.querySelector("[data-js='3button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element3.addEventListener('click', function(){
+	      displayElement.textContent += 3;
+	    })
+	  // This is event listener & callback for - button
+	  var elementSubtract = document.querySelector("[data-js='-button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    elementSubtract.addEventListener('click', function(){
+	      displayElement.textContent += " - ";
+	    })
+	  // This is event listener & callback for 0
+	  var element0 = document.querySelector("[data-js='0button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    element0.addEventListener('click', function(){
+	      displayElement.textContent += 0;
+	    })
+	  // This is event listener & callback for '.' button
+	  var elementPeriod = document.querySelector("[data-js='periodButton']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    elementPeriod.addEventListener('click', function(){
+	      displayElement.textContent += ".";
+	    })
+	  // This is event listener & callback for = button
+	  var elementEqual = document.querySelector("[data-js='=button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    elementEqual.addEventListener('click', function(){
+	      var answer = eval(displayElement.textContent);
+	      displayElement.textContent = answer;
+	    })
+	  // This is event listener & callback for + button
+	  var elementPlus = document.querySelector("[data-js='+button']");
+	  var displayElement = document.querySelector("[data-js='display']");
+	    elementPlus.addEventListener('click', function(){
+	      displayElement.textContent += "+";
+	    })
+	})
+
+
+/***/ }
+/******/ ]);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# assignments
+4.3 -refactor-calculator
+Initail Checklist
+- [x] Make checklist
+- [x] Make a director 4.3 refactor calculator
+- [x] Copy files from 2.4 calculator
+- [x] Initialize npm: npm init in bash
+- [x] Install webpack: npm install webpack --save-dev
+- [x] Install jquery: npm install jquery --save
+- [x] Create webpack.config.js in repo
+- [x] Add "devtool: 'source-map'," in module   object
+- [x] Look up module.exports code placing webpack.config.js
+        locate in 4.3 refactor calculator repo
+- [x] Create git ignore: touch .gitignore
+- [x] Go into atom and add: .DS_Store and node_modules
+- [x] Create second js file inside js directory
+- [ ] webpack --watch
+- [ ] Commit
+
+Create checklist to incorporate refactoring
