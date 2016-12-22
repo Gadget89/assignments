ba0d5b30033ffb57cbaa2c1e1c166e49987adc55
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-22 01:28:59 -0600

    End of day commit with progress on adding to jquery. Needs more work to complete

--- 4.3-refactor-calculator/dist/bundle.js
+++ 4.3-refactor-calculator/dist/bundle.js
@@ -166,19 +166,22 @@
 	$(function() {
 	  // Add data-js to html
 	  // Var(s) to reference (.find) each data-js attribute
-	  var body = $("body");
+	  var $body = $("body");
+	  var $displayWindow = $body.find("[data-js='display']");
+	  var $clearButton = $body.find("[data-js='clearButton']");
+	  var $buttonElement = $body.find("[data-js='button']")
 	  // Click event on button element variables
 	  $buttonElement.on("click", function(e){
 	  // Inside of this anonymous callback function 'this'
 	   //specific button was clicked
-
 	  // Find what was clicked and use jquery
 	  // With the from above... fidn reference to button that
 	    // was clicked
 	  var $clickedButtonElement = $(this);
-	  var $clickedButton = $clickedButtonElement.find("[data-js='']")
-	  // Create...
+	  // Create eval
 
+	  var answer = eval($clickedButtonElement.html());
+	  $displayWindow.html(answer);
 	  })
 	})
 
