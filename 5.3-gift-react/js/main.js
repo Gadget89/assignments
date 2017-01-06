bb3a7f1d7d72c23eb79ae86409522553707c2bd4
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-06 07:32:43 -0600

    Added click events for add button and working

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -4,36 +4,53 @@ import React from 'react'
 
 // All of your UI centered code goes INSIDE of the createClass
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      inputValue: ''
+    };
+  },
+  onAddButtonClick(e) {
+     e.preventDefault();
+    var currentInputValue = this.state.inputValue;
+    // Add var for output to add to bottom of list
+  },
+  updateInputValue: function(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  },
   render() {
-    // Inside of the <h1> using this.state is called BINDING
     return (
       <section className ="body">
-      <h1 className = "title"> GIFTS TO BUY </h1>
-      <main className = "main">
-        <form className = "postForm">
-          <input
-            className = "postForm__text"
-            placeholder = "type the gift to buy here"
-            type = "text">
-          </input>
-          <input
-            className = "addButton"
-            type = "submit"
-            value = "ADD">
-          </input>
-        </form>
-        <section>
-        <article class="posts">
-          <p class="post__text">
-          </p>
-          <p
-            class="total"
-            value="">total
-          </p>
-        </article>
+        <h1 className = "title"> GIFTS TO BUY </h1>
+        <main className = "main">
+          <form className = "postForm">
+            <input
+              className = "postForm__text"
+              onChange={ this.updateInputValue }
+              placeholder = "type the gift to buy here"
+              type = "text"
+              value={ this.state.inputValue }>
+            </input>
+            <input
+              className = "addButton"
+              type = "submit"
+              value = "ADD"
+              onClick={ this.onADDButtonClick }>
+            </input>
+          </form>
+          <section>
+          <article class="posts">
+            <p class="post__text">
+            </p>
+            <p
+              class="total"
+              value="">total
+            </p>
+          </article>
+        </section>
+        </main>
       </section>
-      </main>
-    </section>
     )
   }
 })
