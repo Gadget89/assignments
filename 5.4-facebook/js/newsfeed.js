28b0f025e76af4f2bccd54d22bbbaa60490dbe6a
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-09 16:33:31 -0600

    Added CSS to render according to design

--- 5.4-facebook/js/newsfeed.js
+++ 5.4-facebook/js/newsfeed.js
@@ -10,21 +10,24 @@ export default React.createClass({
     <main className="main">
       <section
         className="article_container">
+        <div>
         <img
           className="author_profile_image"
           src="assets/author_profile_img.jpg">
         </img>
-        <div>
+          <div className="author_info">
           <a className="author_profile_name">
             Instant Articles
           </a>
+          <br></br>
           <p className="article_publish_date">
             May 13, 2015
           </p>
-          <p>
+          </div>
+          <p className="post_content">
             Welcome! To see these stories in the Instant Articles format, you must be on the latest version of the Facebook app for iPhone or Android.
           </p>
-          <p>
+          <p className="post_content">
             Learn more about this new tool for publishers at <a href="#" className="fb_blue">instantarticles.fb.com.</a>
           </p>
         </div>
