b4be458cf98c9cf8609a438e6f07a06816e30c75
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-20 02:26:27 -0600

    Set inital state and included validator to return boolean

--- 7.3-cc-validator/js/main.js
+++ 7.3-cc-validator/js/main.js
@@ -1,2 +1,31 @@
 import React from 'react'
 import Validator from 'validator'
+
+export default React.createClass({
+  // Set initial state
+  getInitialState(){
+    return{
+      isCreditCardValid: false
+    }
+  },
+  onCardChange(e){
+    // Use validator to return boolean from target value
+    var isCreditCard = Validator.isCreditCard(e.target.value);
+    this.setState({isCreditCardValid:isCreditCard})
+  },
+  onSubmit(e){
+    e.preventDefault()
+    var CardValid = this.state.isCreditCardValid;
+    // need to set state here and return with if statement maybe?
+  },
+  render() {
+    return(
+      <section>
+        <form className={this.state.isCreditCardValid ? "valid" : "invalid"}>
+          <input type="text" onChange={this.onCardChange} />
+          <input type="submit"/>
+        </form>
+      </section>
+    )
+  }
+})
