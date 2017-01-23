8633130e322d60fc14ddd8a52a21ebb329c94265
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-01-22 20:45:38 -0600

    Added functions in main.js to get assignment to meet normal mode

--- 7.3-cc-validator/js/main.js
+++ 7.3-cc-validator/js/main.js
@@ -5,27 +5,49 @@ export default React.createClass({
   // Set initial state
   getInitialState(){
     return{
-      isCreditCardValid: false
+      isCreditCardValid: false,
+      messageForOutput: "",
+      newCardInput: ""
     }
   },
+  // This function will run on change to the input form
   onCardChange(e){
-    // Use validator to return boolean from target value
-    var isCreditCard = Validator.isCreditCard(e.target.value);
-    this.setState({isCreditCardValid:isCreditCard})
+    // Create variable to get value from form input
+    var newCreditCard = e.target.value
+    // Set state of new card input
+    this.setState({newCardInput:newCreditCard})
   },
+  // This function will run on submit
   onSubmit(e){
     e.preventDefault()
-    var CardValid = this.state.isCreditCardValid;
-
-    // need to set state here and return with if statement maybe?
+    // Use validator to return boolean from target value
+    var validCard = Validator.isCreditCard(this.state.newCardInput)
+    // Set state to boolean returned from validator
+    this.setState({isCreditCardValid:validCard})
+    // If boolean is false return message for output to "Card Invalid"
+    if (validCard === false) {
+      return this.setState({messageForOutput:"Card Invalid"})
+      // For a boolean of true return message for output to "Card Valid"
+    } else {
+      return this.setState({messageForOutput:"Card Valid"})
+    }
   },
   render() {
     return(
       <section>
+        <p>Please enter credit card number here</p>
         <form>
-          <input className={this.state.isCreditCardValid ? "valid" : "invalid"} type="text" onChange={this.onCardChange} />
-          <input type="submit"/>
+          <input
+            type="text"
+            pattern="[0-9]{16}"
+            maxLength="16"
+            onChange={this.onCardChange} />
+          <input
+            type="submit"
+            onClick={this.onSubmit}/>
         </form>
+        <p
+          className={this.state.isCreditCardValid ? "valid" : "invalid"}>{this.state.messageForOutput}</p>
       </section>
     )
   }
